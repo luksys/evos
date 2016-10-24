5ab6be68a0a11cb9086f0cb6d921373e494c762d
evaldas <nobodiesxx@gmail.com>
2016-10-24 23:18:16 +0100

    added evos latest posts widget

--- lib/widgets.php
+++ lib/widgets.php
@@ -167,5 +167,154 @@ class evos_social_widget extends WP_Widget {
 // Register and load the widget
 function wpb_load_widget() {
     register_widget( 'evos_social_widget' );
+    register_widget( 'evos_widget_recent_posts' );
 }
 add_action( 'widgets_init', 'wpb_load_widget' );
+
+/**
+ * Widget API: WP_Widget_Recent_Posts class
+ *
+ * @package WordPress
+ * @subpackage Widgets
+ * @since 4.4.0
+ */
+/**
+ * Core class used to implement a Recent Posts widget.
+ * 
+ * @since 2.8.0
+ *
+ * @see WP_Widget
+ */
+class Evos_Widget_Recent_Posts extends WP_Widget {
+    /**
+     * Sets up a new Recent Posts widget instance.
+     *
+     * @since 2.8.0
+     * @access public
+     */
+    public function __construct() {
+        $widget_ops = array(
+            'classname' => 'evos_widget_recent_entries',
+            'description' => __( 'Your site&#8217;s most recent Posts.' ),
+            'customize_selective_refresh' => true,
+        );
+        parent::__construct( 'evos_widget_recent_posts', __( 'Evos Recent Posts' ), $widget_ops );
+        $this->alt_option_name = 'evos_widget_recent_entries';
+    }
+    /**
+     * Outputs the content for the current Recent Posts widget instance.
+     *
+     * @since 2.8.0
+     * @access public
+     *
+     * @param array $args     Display arguments including 'before_title', 'after_title',
+     *                        'before_widget', and 'after_widget'.
+     * @param array $instance Settings for the current Recent Posts widget instance.
+     */
+    public function widget( $args, $instance ) {
+        if ( ! isset( $args['widget_id'] ) ) {
+            $args['widget_id'] = $this->id;
+        }
+        $title = ( ! empty( $instance['title'] ) ) ? $instance['title'] : __( 'Recent Posts' );
+        /** This filter is documented in wp-includes/widgets/class-wp-widget-pages.php */
+        $title = apply_filters( 'widget_title', $title, $instance, $this->id_base );
+        $number = ( ! empty( $instance['number'] ) ) ? absint( $instance['number'] ) : 5;
+        if ( ! $number )
+            $number = 5;
+        $show_date = isset( $instance['show_date'] ) ? $instance['show_date'] : false;
+        /**
+         * Filters the arguments for the Recent Posts widget.
+         *
+         * @since 3.4.0
+         *
+         * @see WP_Query::get_posts()
+         *
+         * @param array $args An array of arguments used to retrieve the recent posts.
+         */
+        $r = new WP_Query( apply_filters( 'widget_posts_args', array(
+            'posts_per_page'      => $number,
+            'no_found_rows'       => true,
+            'post_status'         => 'publish',
+            'ignore_sticky_posts' => true
+        ) ) );
+        if ($r->have_posts()) :
+        ?>
+        <?php echo $args['before_widget']; ?>
+        <?php if ( $title ) {
+            echo $args['before_title'] . $title . $args['after_title'];
+        } ?>
+        <ul class="evos-recent-posts">
+        <?php while ( $r->have_posts() ) : $r->the_post(); ?>
+            <li class="item-widget">
+                <div class="frow justify-between">
+                    <?php if( has_post_thumbnail() ) : ?>
+                        <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('thumbnail');?></a>
+                    <?php endif;?>
+                    <div class="item-widget-inner">
+                        <h4 class="item-widget-title">
+                            <a href="<?php the_permalink(); ?>" class="post-title-wrapper">
+                                <?php get_the_title() ? the_title() : the_ID(); ?>
+                            </a>
+                        </h4>
+                         <?php if ( $show_date ) : ?>
+                            <div class="item-post-date">
+                                <i class="fa fa-clock-o icon" aria-hidden="true"></i>
+                                <span class="item-post-date-inner">
+                                    <?php echo get_the_date(); ?>
+                                </span>
+                            </div>
+                        <?php endif; ?>
+                    <span class="item-widget-content"><?php echo substr(get_the_excerpt(), 0, 80);?>...</span> 
+                    </div>
+                    </div>
+            </li>
+        <?php endwhile; ?>
+        </ul>
+        <?php echo $args['after_widget']; ?>
+        <?php
+        // Reset the global $the_post as this query will have stomped on it
+        wp_reset_postdata();
+        endif;
+    }
+    /**
+     * Handles updating the settings for the current Recent Posts widget instance.
+     *
+     * @since 2.8.0
+     * @access public
+     *
+     * @param array $new_instance New settings for this instance as input by the user via
+     *                            WP_Widget::form().
+     * @param array $old_instance Old settings for this instance.
+     * @return array Updated settings to save.
+     */
+    public function update( $new_instance, $old_instance ) {
+        $instance = $old_instance;
+        $instance['title'] = sanitize_text_field( $new_instance['title'] );
+        $instance['number'] = (int) $new_instance['number'];
+        $instance['show_date'] = isset( $new_instance['show_date'] ) ? (bool) $new_instance['show_date'] : false;
+        return $instance;
+    }
+    /**
+     * Outputs the settings form for the Recent Posts widget.
+     *
+     * @since 2.8.0
+     * @access public
+     *
+     * @param array $instance Current settings.
+     */
+    public function form( $instance ) {
+        $title     = isset( $instance['title'] ) ? esc_attr( $instance['title'] ) : '';
+        $number    = isset( $instance['number'] ) ? absint( $instance['number'] ) : 5;
+        $show_date = isset( $instance['show_date'] ) ? (bool) $instance['show_date'] : false;
+?>
+        <p><label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label>
+        <input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo $title; ?>" /></p>
+
+        <p><label for="<?php echo $this->get_field_id( 'number' ); ?>"><?php _e( 'Number of posts to show:' ); ?></label>
+        <input class="tiny-text" id="<?php echo $this->get_field_id( 'number' ); ?>" name="<?php echo $this->get_field_name( 'number' ); ?>" type="number" step="1" min="1" value="<?php echo $number; ?>" size="3" /></p>
+
+        <p><input class="checkbox" type="checkbox"<?php checked( $show_date ); ?> id="<?php echo $this->get_field_id( 'show_date' ); ?>" name="<?php echo $this->get_field_name( 'show_date' ); ?>" />
+        <label for="<?php echo $this->get_field_id( 'show_date' ); ?>"><?php _e( 'Display post date?' ); ?></label></p>
+<?php
+    }
+}
